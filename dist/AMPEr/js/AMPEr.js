c01cea1144c30fcb207b4af25bafa22fa3dc03f2
Bob Vrijland <b.vrijland@esens.nl>
2020-11-05 17:57:33 +0100

    Changed to rem in the css, so everythings scale with the html font size ;)

--- dist/AMPEr/js/AMPEr.js
+++ dist/AMPEr/js/AMPEr.js
@@ -32,7 +32,7 @@ var AMPEr = function () {
         modalTitle: "<i class='AMPEr_icon--rozekoek' aria-hidden='true'></i>Cookies?",
         modalContent: "This is body text for giving a little bit more information about cookies in the modal ENGELS",
         acceptBtn: "Accept all <span class='sr-only'>cookies and close this popup.</span>",
-        settingsBtn: "<span class='sr-only'>Settings</span><i class='AMPEr_icon--gear' aria-hidden='true'></i>",
+        settingsBtn: "<span class='sr-only'>Adjust cookie Settings</span><i class='AMPEr_icon--gear' aria-hidden='true'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z'/></svg></i>",
         saveBtn: "Save <span class='sr-only'>cookie settings and close the this popup.</span>",
         backBtn: "Back <span class='sr-only'>to first screen.</span>",
         analytic: "Analytic cookies",
@@ -46,14 +46,14 @@ var AMPEr = function () {
         acceptBtn: "Accepteer alles <span class='sr-only'>qua cookies en sluit deze popup.</span>",
         saveBtn: "<span class='sr-only'>Instellingen</span> Opslaan <span class='sr-only'>en deze popup sluiten.</span>",
         backBtn: "Terug <span class='sr-only'>Naar het vorige scherm</span>",
-        settingsBtn: "<span class='sr-only'>Instellingen</span><i class='AMPEr_icon--gear' aria-hidden='true'></i>",
+        settingsBtn: "<span class='sr-only'>Cookie instellingen wijzigen</span><i class='AMPEr_icon--gear' aria-hidden='true'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z'/></svg></i>",
         analytic: "Analytische cookies",
         marketing: "Marketing cookies",
         personalization: "Personalisatie cookies",
         essential: "Functionele cookies"
       }
     },
-    // @TODO: Maak multilingual
+    // @TODO: Maak onderstaande cookie configuratie multilingual en misschien een ander bestand zodat het makkelijk aan te passen is ipv inline
     cookies: {
       analytic: {
         vb1: {
@@ -104,8 +104,8 @@ var AMPEr = function () {
   /**
    * Make the banner / modal, attach listeners and attacht it to the body
    * @TODO: Switch to settings and back, make it accesible with a focus switch?
-   * @TODO: Show settings only if there is a cookie for it
-   * @TODO: Make the information HTML
+   * @TODO: Show settings only if there is a cookie for it ??
+   * @TODO: Make the information HTML ?
    */
 
   var showCookieWindow = function showCookieWindow() {
