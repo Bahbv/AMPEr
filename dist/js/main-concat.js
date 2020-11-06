515bcdcca05a3c31a678e5861c736616f3b89bbf
Bob Vrijland <b.vrijland@esens.nl>
2020-11-06 11:08:04 +0100

    Added ghost buttons and accesible focus style (When not using mouse)

--- dist/js/main-concat.js
+++ dist/js/main-concat.js
@@ -4,9 +4,25 @@
 "use strict";
 
 function main() {
+  a11yFocus();
   AMPEr.init({
     debugMode: true
   });
 }
+
+var a11yFocus = function a11yFocus() {
+  // Initially add the using mouse class, because most people do.
+  document.documentElement.classList.add('using-mouse'); // Let the document know when the mouse is being used
+
+  document.body.addEventListener('mousedown', function () {
+    document.documentElement.classList.add('using-mouse');
+  }); // Re-enable focus styling when Tab is pressed
+
+  document.body.addEventListener('keydown', function (event) {
+    if (event.keyCode === 9) {
+      document.documentElement.classList.remove('using-mouse');
+    }
+  });
+};
 
 main();
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,26 @@
-function main(){
+function main() {
+    a11yFocus();
+
+
     AMPEr.init({
         debugMode: true,
-    });    
+    });
 }
+
+const a11yFocus = function () {
+    // Initially add the using mouse class, because most people do.
+    document.documentElement.classList.add('using-mouse');
+    // Let the document know when the mouse is being used
+    document.body.addEventListener('mousedown', function () {
+        document.documentElement.classList.add('using-mouse');
+    });
+    // Re-enable focus styling when Tab is pressed
+    document.body.addEventListener('keydown', function (event) {
+        if (event.keyCode === 9) {
+            document.documentElement.classList.remove('using-mouse');
+        }
+    });
+}
+
 
 main();
